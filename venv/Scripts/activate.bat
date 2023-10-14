d2cb9c19316516bf5aa986855af9746ec69d0f06
Guillaume <guillaume.baroin@etudiant.univ-reims.fr>
2023-10-14 20:26:31 +0200

    mise à jour venv 3.12.0

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Guillaume Baroin\Documents\M2_sep\DIGITAL_PROJECT3\venv
+set VIRTUAL_ENV=C:\Users\Guillaume\Documents\DIGITAL_PROJECT3\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
