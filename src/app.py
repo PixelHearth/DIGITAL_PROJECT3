7eaf865c57a182b4049584fb59abd93b5b76a157
Guillaume <guillaume.baroin@etudiant.univ-reims.fr>
2023-10-16 18:37:30 +0200

    création du résultat fait

--- src/app.py
+++ src/app.py
@@ -6,22 +6,22 @@ import pandas as pd
 import numpy as np
 def app():
     properties = generate_property_data(100)
-    
-    cpp = CustomPreprocessor()
-    cpp.fit(properties)
-    cpp.transform(properties)
-    
     new_variable = generate_property_data(1)
-    print(new_variable)
-    cpp.transform(new_variable)
-    #! trouver un moyen de convertir les variables string en variables ordinal avec Ordinal encoder
+    cpp_p = CustomPreprocessor(properties)
+    cpp_p.fit_transform()
+    cpp_p.transform(new_variable)
+    cpp_p.transform(properties)
+    # ! trouver un moyen de convertir les variables string en variables ordinal avec Ordinal encoder
     predict = k_neighbors(properties,new_variable)
     independante_variable =new_variable.iloc[:,1:].values.flatten()
     reel =new_variable.values.flatten()
     result = np.concatenate([predict, independante_variable])
-    print(reel)
-    print(result)
-    cpp.encoder.inverse_transform(result)
+    df_reel = pd.DataFrame(reel).transpose()
+    cpp_p.inverse_transform(df_reel)
+    print(df_reel)
+    df_result = pd.DataFrame(result).transpose()
+    cpp_p.inverse_transform(df_result)
+    print(df_result)
     
 if __name__ == "__main__":
     app()
