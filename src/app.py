a398559432ac4fb3deb0d10eec5e64251612cba4
Guillaume <guillaume.baroin@etudiant.univ-reims.fr>
2023-10-17 12:57:25 +0200

    optimisation du code

--- src/app.py
+++ src/app.py
@@ -1,27 +1,25 @@
 from data.make_dataset import generate_property_data
 from data.preprocessing import CustomPreprocessor
-from models.train_model import k_neighbors
+from models.train_model import KNN_MODEL
 import random
 import pandas as pd
 import numpy as np
 def app():
     properties = generate_property_data(100)
     new_variable = generate_property_data(1)
+    # print(new_variable)
     cpp_p = CustomPreprocessor(properties)
     cpp_p.fit()
     cpp_p.transform(new_variable)
     cpp_p.transform(properties)
     # ! trouver un moyen de convertir les variables string en variables ordinal avec Ordinal encoder
-    predict = k_neighbors(properties,new_variable)
-    independante_variable =new_variable.iloc[:,1:].values.flatten()
+    individual = KNN_MODEL(properties,new_variable).k_neighbors()
+    cpp_p.inverse_transform(individual)
+    print(individual)
     reel =new_variable.values.flatten()
-    result = np.concatenate([predict, independante_variable])
     df_reel = pd.DataFrame(reel).transpose()
     cpp_p.inverse_transform(df_reel)
     print(df_reel)
-    df_result = pd.DataFrame(result).transpose()
-    cpp_p.inverse_transform(df_result)
-    print(df_result)
     
 if __name__ == "__main__":
     app()
