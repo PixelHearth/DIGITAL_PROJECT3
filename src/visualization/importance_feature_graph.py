900f15799844c19ade53a968897574e92943b31f
PixelHeart <guillaume.baroin@etudiant.univ-reims.fr>
2023-11-01 21:48:43 +0100

    ajout fonction graph selection variable

---
+++ src/visualization/importance_feature_graph.py
@@ -0,0 +1,46 @@
+
+import matplotlib.pyplot as plt
+def plot_feature_importance(importances_df, nb_feature):
+    """
+    Crée un graphique à barres montrant l'importance des caractéristiques dans un modèle.
+
+    Args:
+        importances_df (DataFrame): Le DataFrame contenant les informations sur l'importance des caractéristiques.
+        nb_feature (int): Le nombre de caractéristiques à inclure dans le graphique en fonction de leur importance.
+
+    Returns:
+        None
+
+    Ce graphique affiche l'importance des caractéristiques dans un modèle. Les caractéristiques sont affichées sur l'axe des x
+    et leur importance sur l'axe des y. Le pourcentage total d'importance représenté par les caractéristiques les plus
+    importantes est également indiqué.
+
+    Args:
+        importances_df (DataFrame): Le DataFrame contenant les informations sur l'importance des caractéristiques.
+        nb_feature (int): Le nombre de caractéristiques à inclure dans le graphique en fonction de leur importance.
+
+    Returns:
+        None
+    """
+    # Créer le graphique à barres
+    plt.figure(figsize=(10, 6))
+    plt.bar(importances_df['Feature'], importances_df['Importance'], color='b')
+
+    # Titre du graphique, Nom des axes
+    plt.xlabel('Caractéristique')
+    plt.ylabel('Importance')
+    plt.title('Importance des Caractéristiques dans le Modèle Random Forest')
+
+    # Lisibilité du graphique
+    plt.xticks(rotation=45)  
+    plt.tight_layout()
+
+    # Calcul du pourcentage total d'importance représenté par les caractéristiques les plus importantes
+    total_importance = importances_df['Importance'].sum()
+    top_features_importance = importances_df.iloc[:nb_feature]['Importance'].sum()  
+    percentage = (top_features_importance / total_importance) * 100
+
+    # Ajout du texte indiquant le pourcentage total d'importance représenté par les caractéristiques les plus importantes
+    plt.annotate(f"Top {nb_feature} Features: {percentage:.2f}%", xy=(0.5, 0.9), xycoords='axes fraction')
+    plt.savefig("report/features_importance.png")
+
