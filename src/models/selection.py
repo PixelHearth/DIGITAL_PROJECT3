dffd91bea9f7c067d742b0a867d24993ab8bff9d
PixelHeart <guillaume.baroin@etudiant.univ-reims.fr>
2023-11-01 21:48:19 +0100

    ajout doc

--- src/models/selection.py
+++ src/models/selection.py
@@ -1,45 +1,44 @@
-from sklearn.ensemble import RandomForestClassifier
-import numpy as np
 import pandas as pd
-            
-def select_variables(dataframe,individual_features):
-    #initialisation des variable
+from sklearn.ensemble import RandomForestClassifier
 
-    dependent_variable = dataframe.iloc[:,0]
-    independent_variable  = dataframe.iloc[:,1:]
-    individual_independante_features  = individual_features.iloc[:,1:]
-    rf_model = RandomForestClassifier(n_estimators=100,random_state=42)
-    rf_model.fit(independent_variable.values,dependent_variable.values)
+def select_variables(dataframe, individual_features, nb_feature):
+    """
+    Sélectionne les variables les plus explicatives du modèle à l'aide d'un modèle RandomForest.
 
+    Parameters:
+    dataframe (DataFrame): Le DataFrame contenant les données.
+    individual_features (DataFrame): Le DataFrame des caractéristiques individuelles.
+    nb_feature (int): Le nombre de caractéristiques à sélectionner.
+
+    Returns:
+    selected_dataframe (DataFrame): Le DataFrame contenant les caractéristiques sélectionnées.
+    selected_new_variable (DataFrame): Le DataFrame des caractéristiques individuelles correspondantes.
+    selected_new_variable_col (Index): L'Index des colonnes de selected_new_variable.
+    importances_df (DataFrame): Le DataFrame des importances des caractéristiques.
+    """
+    # Extraction des variables dépendantes et indépendantes
+    dependent_variable = dataframe.iloc[:, 0]
+    independent_variable = dataframe.iloc[:, 1:]
+    individual_independante_features = individual_features.iloc[:, 1:]
+
+    # Entraînement d'un modèle Random Forest
+    rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+    rf_model.fit(independent_variable.values, dependent_variable.values)
+
+    # Calcul des importances des caractéristiques
     feature_importances = rf_model.feature_importances_
-    importances_df = pd.DataFrame({'Feature': dataframe.iloc[:,1:].columns, 'Importance': feature_importances})
+    importances_df = pd.DataFrame({'Feature': dataframe.iloc[:, 1:].columns, 'Importance': feature_importances})
     importances_df = importances_df.sort_values(by='Importance', ascending=False)
-    selection_dataframe = importances_df[:10]
 
-    
-    # restitution des index et des noms des colonnes transformées
+    # Sélection des caractéristiques en fonction de nb_feature
+    selection_dataframe = importances_df[:nb_feature]
 
+    # Récupération des noms des caractéristiques sélectionnées
     selected_features = selection_dataframe['Feature'].tolist()
-    selected_dataframe = pd.concat([dependent_variable, independent_variable[selected_features]],axis = 1)
-    selected_new_variable = pd.concat([individual_features.iloc[:,0],individual_independante_features[selected_features]],axis = 1)
+
+    # Création du DataFrame résultant avec les caractéristiques sélectionnées
+    selected_dataframe = pd.concat([dependent_variable, independent_variable[selected_features]], axis=1)
+    selected_new_variable = pd.concat([individual_features.iloc[:, 0], individual_independante_features[selected_features]], axis=1)
     selected_new_variable_col = selected_new_variable.columns
-    return selected_dataframe,selected_new_variable,selected_new_variable_col
 
-
-    
-    
-    # # Créer le graphique à barres
-    # plt.figure(figsize=(10, 6))
-    # plt.bar(importances_df['Feature'], importances_df['Importance'], color='b')
-    # plt.xlabel('Caractéristique')
-    # plt.ylabel('Importance')
-    # plt.title('Importance des Caractéristiques dans le Modèle Random Forest')
-    # plt.xticks(rotation=45)  # Rotation des étiquettes pour une meilleure lisibilité
-    # plt.tight_layout()
-
-    # # Afficher le pourcentage total d'importance représenté par les caractéristiques les plus importantes
-    # total_importance = importances_df['Importance'].sum()
-    # top_features_importance = importances_df.iloc[:nb_feature]['Importance'].sum()  # Par exemple, en prenant les 3 premières caractéristiques
-    # percentage = (top_features_importance / total_importance) * 100
-    # plt.annotate(f"Top {nb_feature} Features: {percentage:.2f}%", xy=(0.5, 0.9), xycoords='axes fraction')
-    # plt.show()
+    return selected_dataframe, selected_new_variable, selected_new_variable_col, importances_df
