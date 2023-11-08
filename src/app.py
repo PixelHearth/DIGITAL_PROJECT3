179949c3ad249c6fb67ebd62923ae06fed252c61
gbar-dev <baroin.guillaume.pro@gmail.com>
2023-11-08 12:45:16 +0100

    ajout excel output

--- src/app.py
+++ src/app.py
@@ -6,48 +6,60 @@ from data.CleanBDD import clean
 from data.make_dataset import importation_excel
 import pandas as pd
 import time
+import os
+
 def app():
     # calcul le temps du début
     start = time.time()
 
     # import bdd
-    properties = clean("C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/data/processed/Base_clean.csv")
-    # properties = pd.read_csv("C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/data/processed/bdd_model.csv", index_col="Unnamed: 0")
+    properties = clean("C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/data/raw/Base_clean.csv")
+    new_variable = importation_excel("C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/essai.xlsm", "saisie")
     #selection d'une variable pour le test
-    new_variable = importation_excel("C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/essai.xlsm","saisie")
-     
+    # new_variable = properties.sample(1)
+
     #instance du framework de processing et entrainement des données sur properties pour l'encodage
-    cpp_p = CustomPreprocessor(properties)
-    cpp_p.fit()
+    cpp_p_selection = CustomPreprocessor(properties)
+    cpp_p_selection.fit()
+
     #encodage des variables dans les deux bases de données
-    cpp_p.transform(new_variable)
-    cpp_p.transform(properties)
+    cpp_p_selection.transform(properties)
 
     #selection des variables importantes, il faut avoir fait l'encodage aupréalable
     nb_features = 10
-    properties,new_variable,nv_colonne,importance = select_variables(properties,new_variable,nb_features)
+    properties,importance = select_variables(properties,nb_features)
+    cpp_p_selection.inverse_transform(properties)
+
+    #instance du framework de processing et entrainement des données sur properties pour l'encodage après selection
+    cpp_kneigh = CustomPreprocessor(properties)
+    cpp_kneigh.fit()
+
+    cpp_kneigh.transform(properties)
     
+    cpp_kneigh.transform(new_variable)
+    
+
     #création du graph des importances dans le modèle de selection
-    plot_feature_importance(importance,10)
+    # plot_feature_importance(importance,10)
+
     #instance et entrainement du k_neighbors sur les données encodées 
     individual = Models(properties,new_variable).k_neighbors()
-    individual.columns = nv_colonne
-
+    individual.columns = new_variable.columns
     #restitution d'un dataframe compréhensible pour un humain
     #valeur prédite du k_neighbors
-    cpp_p.inverse_transform(individual)
+    cpp_kneigh.inverse_transform(individual)
+    individual = individual.iloc[:,0].values.flatten()[0]
+    chemin_fichier = os.path.join('data/processed', 'prediction.txt')
 
-    #valeur réelle de l'individu testé
-    cpp_p.inverse_transform(new_variable)
+    # Ouvrez le fichier en mode écriture (w), s'il n'existe pas, il sera créé
+    with open(chemin_fichier, 'w') as fichier_texte:
+        # Écrivez la chaîne dans le fichier
+        fichier_texte.write(individual)
 
-    #comparaison avec la valeur réelle
-    print(individual)
-    print(new_variable)
 
     #calcul du temps d'exécution total
     end = time.time()
     print("Temps d'exécution : ",round(end-start,2),"secondes") 
-
     return individual
     
 if __name__ == "__main__":
