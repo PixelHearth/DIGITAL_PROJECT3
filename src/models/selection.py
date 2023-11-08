3a67a9cd366d0af17c0bd94db3d788683a7424de
gbar-dev <baroin.guillaume.pro@gmail.com>
2023-11-08 12:42:51 +0100

    aucun changement

--- src/models/selection.py
+++ src/models/selection.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-def select_variables(dataframe, individual_features, nb_feature):
+def select_variables(dataframe, nb_feature):
     """
     Sélectionne les variables les plus explicatives du modèle à l'aide d'un modèle RandomForest.
 
@@ -18,12 +18,9 @@ def select_variables(dataframe, individual_features, nb_feature):
     """
     #création d'assert préalable 
     assert isinstance(dataframe,pd.DataFrame)
-    assert isinstance(individual_features,pd.DataFrame)
-    assert individual_features.shape[1] != dataframe.shape[1]
-
+    # assert individual_features.shape[1] == dataframe.shape[1], "les dimensions des bases de données doivent être égales"
 
     assert isinstance(nb_feature,int)
-    assert nb_feature < len(dataframe.columns) or nb_feature< len(individual_features.columns)
     assert nb_feature > 0, "nb_feature doit être supérieur à zéro"
     assert nb_feature <= len(dataframe.columns) - 1, "nb_feature ne doit pas dépasser le nombre de colonnes dans le dataframe - 1"
    
@@ -31,7 +28,6 @@ def select_variables(dataframe, individual_features, nb_feature):
     # Extraction des variables dépendantes et indépendantes
     dependent_variable = dataframe.iloc[:, 0]
     independent_variable = dataframe.iloc[:, 1:]
-    individual_independante_features = individual_features.iloc[:, 1:]
 
     # Entraînement d'un modèle Random Forest
     rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -51,7 +47,5 @@ def select_variables(dataframe, individual_features, nb_feature):
 
     # Création du DataFrame résultant avec les caractéristiques sélectionnées
     selected_dataframe = pd.concat([dependent_variable, independent_variable[selected_features]], axis=1)
-    selected_new_variable = pd.concat([individual_features.iloc[:, 0], individual_independante_features[selected_features]], axis=1)
-    selected_new_variable_col = selected_new_variable.columns
 
-    return selected_dataframe, selected_new_variable, selected_new_variable_col, importances_df
+    return selected_dataframe, importances_df
