7f28f6ee9a90e42dc6b0ebff086344199c325878
Armand51 <121493302+Armand51@users.noreply.github.com>
2023-11-22 18:39:31 +0100

    Update clean.py
    passage des fonctions de filtrage en anglais

--- src/data/clean.py
+++ src/data/clean.py
@@ -46,101 +46,101 @@ def clean(chemin):
 
 
     #put classe_bilan_dpe to first column
-    df = deplacer_colonne_en_premier(df, 'classe_bilan_dpe')   
+    df = switch_first_column(df, 'classe_bilan_dpe')   
 
     #keep columns useful
-    df = selectionner_colonnes(df, colonnes)
+    df = select_columns(df, colonnes)
 
     #take a near modality to descrease number of modality
     #type_adjacence_principal_plancher_haut
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_haut", "comble faiblement ventilÃ©", "comble")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_haut", "comble fortement ventilÃ©", "comble")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_haut", "comble trÃ¨s faiblement ventilÃ©", "comble")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_haut", "circulation avec ouverture directe sur l'extÃ©rieur", "circulation")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_haut", "circulation sans ouverture directe sur l'extÃ©rieur", "circulation")
+    df = replace_value(df, "type_adjacence_principal_plancher_haut", "comble faiblement ventilÃ©", "comble")
+    df = replace_value(df, "type_adjacence_principal_plancher_haut", "comble fortement ventilÃ©", "comble")
+    df = replace_value(df, "type_adjacence_principal_plancher_haut", "comble trÃ¨s faiblement ventilÃ©", "comble")
+    df = replace_value(df, "type_adjacence_principal_plancher_haut", "circulation avec ouverture directe sur l'extÃ©rieur", "circulation")
+    df = replace_value(df, "type_adjacence_principal_plancher_haut", "circulation sans ouverture directe sur l'extÃ©rieur", "circulation")
 
     #local_non_chauffe_principal_plancher_haut#
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_haut", "hall d'entrÃ©e avec dispositif de fermeture automatique", "hall")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_haut", "hall d'entrÃ©e sans dispositif de fermeture automatique", "hall")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_haut", "circulation avec ouverture directe sur l'extÃ©rieur", "circulation")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_haut", "circulation sans ouverture directe sur l'extÃ©rieur", "circulation")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_haut", "circulation avec bouche ou gaine de dÃ©senfumage ouverte en permanence", "circulation")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_haut", "comble faiblement ventilÃ©", "comble")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_haut", "comble trÃ¨s faiblement ventilÃ©", "comble")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_haut", "comble fortement ventilÃ©", "comble")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_haut", "hall d'entrÃ©e avec dispositif de fermeture automatique", "hall")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_haut", "hall d'entrÃ©e sans dispositif de fermeture automatique", "hall")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_haut", "circulation avec ouverture directe sur l'extÃ©rieur", "circulation")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_haut", "circulation sans ouverture directe sur l'extÃ©rieur", "circulation")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_haut", "circulation avec bouche ou gaine de dÃ©senfumage ouverte en permanence", "circulation")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_haut", "comble faiblement ventilÃ©", "comble")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_haut", "comble trÃ¨s faiblement ventilÃ©", "comble")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_haut", "comble fortement ventilÃ©", "comble")
 
     #type_plancher_haut_deperditif
-    df = remplacer_valeurs(df, "type_plancher_haut_deperditif", "autre type de plafond non rÃ©pertoriÃ©", "autre")
-    df = remplacer_valeurs(df, "type_plancher_haut_deperditif", "bardeaux et remplissage", "autre")
-    df = remplacer_valeurs(df, "type_plancher_haut_deperditif", "plafond bois sous solives mÃ©talliques", "autre")
-    df = remplacer_valeurs(df, "type_plancher_haut_deperditif", "plafond bois sur solives mÃ©talliques", "autre")
-    df = remplacer_valeurs(df, "type_plancher_haut_deperditif", "plafond entre solives mÃ©talliques avec ou sans remplissage", "autre")
-    df = remplacer_valeurs(df, "type_plancher_haut_deperditif", "plafond lourd type entrevous terre-cuite, poutrelles bÃ©ton", "autre")
-    df = remplacer_valeurs(df, "type_plancher_haut_deperditif", "toitures en Bac acier", "autre")
+    df = replace_value(df, "type_plancher_haut_deperditif", "autre type de plafond non rÃ©pertoriÃ©", "autre")
+    df = replace_value(df, "type_plancher_haut_deperditif", "bardeaux et remplissage", "autre")
+    df = replace_value(df, "type_plancher_haut_deperditif", "plafond bois sous solives mÃ©talliques", "autre")
+    df = replace_value(df, "type_plancher_haut_deperditif", "plafond bois sur solives mÃ©talliques", "autre")
+    df = replace_value(df, "type_plancher_haut_deperditif", "plafond entre solives mÃ©talliques avec ou sans remplissage", "autre")
+    df = replace_value(df, "type_plancher_haut_deperditif", "plafond lourd type entrevous terre-cuite, poutrelles bÃ©ton", "autre")
+    df = replace_value(df, "type_plancher_haut_deperditif", "toitures en Bac acier", "autre")
 
     #type_adjacence_principal_plancher_bas
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_bas", "circulation avec bouche ou gaine de désenfumage ouverte en permanence", "circulation")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_bas", "circulation avec ouverture directe sur l'extérieur", "circulation")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_bas", "circulation sans ouverture directe sur l'extérieur", "circulation")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_bas", "hall d'entrée avec dispositif de fermeture automatique", "hall")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_bas", "hall d'entrée sans dispositif de fermeture automatique", "hall")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_bas", "espace tampon solarisé (véranda, loggia fermée)", "autres dépendances")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_bas", "garage privé collectif", "garage")
-    df = remplacer_valeurs(df, "type_adjacence_principal_plancher_bas", "cellier", "autres dépendances")
+    df = replace_value(df, "type_adjacence_principal_plancher_bas", "circulation avec bouche ou gaine de désenfumage ouverte en permanence", "circulation")
+    df = replace_value(df, "type_adjacence_principal_plancher_bas", "circulation avec ouverture directe sur l'extérieur", "circulation")
+    df = replace_value(df, "type_adjacence_principal_plancher_bas", "circulation sans ouverture directe sur l'extérieur", "circulation")
+    df = replace_value(df, "type_adjacence_principal_plancher_bas", "hall d'entrée avec dispositif de fermeture automatique", "hall")
+    df = replace_value(df, "type_adjacence_principal_plancher_bas", "hall d'entrée sans dispositif de fermeture automatique", "hall")
+    df = replace_value(df, "type_adjacence_principal_plancher_bas", "espace tampon solarisé (véranda, loggia fermée)", "autres dépendances")
+    df = replace_value(df, "type_adjacence_principal_plancher_bas", "garage privé collectif", "garage")
+    df = replace_value(df, "type_adjacence_principal_plancher_bas", "cellier", "autres dépendances")
 
     #local_non_chauffe_principal_plancher_bas
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_bas", "circulation avec bouche ou gaine de désenfumage ouverte en permanence", "circulation")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_bas", "circulation avec ouverture directe sur l'extérieur", "circulation")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_bas", "circulation sans ouverture directe sur l'extérieur", "circulation")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_bas", "hall d'entrée avec dispositif de fermeture automatique", "hall")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_bas", "hall d'entrée sans dispositif de fermeture automatique", "hall")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_bas", "espace tampon solarisé (véranda, loggia fermée)", "autres dépendances")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_bas", "garage privé collectif", "garage")
-    df = remplacer_valeurs(df, "local_non_chauffe_principal_plancher_bas", "cellier", "autres dépendances")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_bas", "circulation avec bouche ou gaine de désenfumage ouverte en permanence", "circulation")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_bas", "circulation avec ouverture directe sur l'extérieur", "circulation")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_bas", "circulation sans ouverture directe sur l'extérieur", "circulation")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_bas", "hall d'entrée avec dispositif de fermeture automatique", "hall")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_bas", "hall d'entrée sans dispositif de fermeture automatique", "hall")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_bas", "espace tampon solarisé (véranda, loggia fermée)", "autres dépendances")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_bas", "garage privé collectif", "garage")
+    df = replace_value(df, "local_non_chauffe_principal_plancher_bas", "cellier", "autres dépendances")
 
     #materiaux_structure_mur_exterieur
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs bois (rondin)", "murs en pan de bois")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en pan de bois sans remplissage tout venant", "murs en pan de bois")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en pan de bois avec remplissage tout venant", "murs en pan de bois")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en ossature bois avec isolant en remplissage <2001", "murs en ossature bois")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en ossature bois avec isolant en remplissage 2001-2005", "murs en ossature bois")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en ossature bois avec isolant en remplissage â‰¥ 2006", "murs en ossature bois")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en ossature bois avec remplissage tout venant", "murs en ossature bois")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en ossature bois sans remplissage", "murs en ossature bois")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "bÃ©ton cellulaire", "murs en béton")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en bÃ©ton banchÃ©", "murs en béton")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en bÃ©ton de mÃ¢chefer", "murs en béton")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "brique terre cuite alvÃ©olaire", "autre matÃ©riau non rÃ©pertoriÃ©")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "cloison de plÃ¢tre", "autre matÃ©riau non rÃ©pertoriÃ©")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs en ossature bois", "autre matÃ©riau non rÃ©pertoriÃ©")
-    df = remplacer_valeurs(df, "materiaux_structure_mur_exterieur", "murs sandwich bÃ©ton/isolant/bÃ©ton (sans isolation rapportÃ©e)", "autre matÃ©riau non rÃ©pertoriÃ©")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs bois (rondin)", "murs en pan de bois")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en pan de bois sans remplissage tout venant", "murs en pan de bois")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en pan de bois avec remplissage tout venant", "murs en pan de bois")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en ossature bois avec isolant en remplissage <2001", "murs en ossature bois")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en ossature bois avec isolant en remplissage 2001-2005", "murs en ossature bois")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en ossature bois avec isolant en remplissage â‰¥ 2006", "murs en ossature bois")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en ossature bois avec remplissage tout venant", "murs en ossature bois")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en ossature bois sans remplissage", "murs en ossature bois")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "bÃ©ton cellulaire", "murs en béton")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en bÃ©ton banchÃ©", "murs en béton")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en bÃ©ton de mÃ¢chefer", "murs en béton")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "brique terre cuite alvÃ©olaire", "autre matÃ©riau non rÃ©pertoriÃ©")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "cloison de plÃ¢tre", "autre matÃ©riau non rÃ©pertoriÃ©")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs en ossature bois", "autre matÃ©riau non rÃ©pertoriÃ©")
+    df = replace_value(df, "materiaux_structure_mur_exterieur", "murs sandwich bÃ©ton/isolant/bÃ©ton (sans isolation rapportÃ©e)", "autre matÃ©riau non rÃ©pertoriÃ©")
 
     #type_ventilation
-    df = remplacer_valeurs(df, "type_ventilation", "Ventilation mécanique double flux avec échangeur", "Ventilation mécanique double flux")
-    df = remplacer_valeurs(df, "type_ventilation", "Ventilation mécanique double flux sans échangeur", "Ventilation mécanique double flux")
+    df = replace_value(df, "type_ventilation", "Ventilation mécanique double flux avec échangeur", "Ventilation mécanique double flux")
+    df = replace_value(df, "type_ventilation", "Ventilation mécanique double flux sans échangeur", "Ventilation mécanique double flux")
 
     #type_generateur_ecs
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere fioul basse temperature", "chaudiere fioul")
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere fioul condensation", "chaudiere fioul")
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere fioul standard", "chaudiere fioul")
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere gaz basse temperature", "chaudiere gaz")
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere gaz condensation", "chaudiere gaz")
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere gaz standard", "chaudiere gaz")
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere gpl/butane/propane basse temperature", "chaudiere gpl/butane/propane")
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere gpl/butane/propane condensation", "chaudiere gpl/butane/propane")
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere gpl/butane/propane standard", "chaudiere gpl/butane/propane")
-    df = remplacer_valeurs(df, "type_generateur_ecs", "chaudiere gpl/butane/propaneindependant", "chaudiere gpl/butane/propane")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere fioul basse temperature", "chaudiere fioul")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere fioul condensation", "chaudiere fioul")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere fioul standard", "chaudiere fioul")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere gaz basse temperature", "chaudiere gaz")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere gaz condensation", "chaudiere gaz")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere gaz standard", "chaudiere gaz")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere gpl/butane/propane basse temperature", "chaudiere gpl/butane/propane")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere gpl/butane/propane condensation", "chaudiere gpl/butane/propane")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere gpl/butane/propane standard", "chaudiere gpl/butane/propane")
+    df = replace_value(df, "type_generateur_ecs", "chaudiere gpl/butane/propaneindependant", "chaudiere gpl/butane/propane")
 
     #type_generateur_chauffage
-    df = remplacer_valeurs(df, "type_generateur_chauffage", "chaudiere fioul basse temperature", "chaudiere fioul")
-    df = remplacer_valeurs(df, "type_generateur_chauffage", "chaudiere fioul condensation", "chaudiere fioul")
-    df = remplacer_valeurs(df, "type_generateur_chauffage", "chaudiere fioul standard", "chaudiere fioul")
-    df = remplacer_valeurs(df, "type_generateur_chauffage", "chaudiere gaz basse temperature", "chaudiere gaz")
-    df = remplacer_valeurs(df, "type_generateur_chauffage", "chaudiere gaz condensation", "chaudiere gaz")
-    df = remplacer_valeurs(df, "type_generateur_chauffage", "chaudiere gaz standard", "chaudiere gaz")
-    df = remplacer_valeurs(df, "type_generateur_chauffage", "chaudiere gpl/butane/propane basse temperature", "chaudiere gpl/butane/propane")
-    df = remplacer_valeurs(df, "type_generateur_chauffage", "chaudiere gpl/butane/propane condensation", "chaudiere gpl/butane/propane")
-    df = remplacer_valeurs(df, "type_generateur_chauffage", "chaudiere gpl/butane/propane standard", "chaudiere gpl/butane/propane")
+    df = replace_value(df, "type_generateur_chauffage", "chaudiere fioul basse temperature", "chaudiere fioul")
+    df = replace_value(df, "type_generateur_chauffage", "chaudiere fioul condensation", "chaudiere fioul")
+    df = replace_value(df, "type_generateur_chauffage", "chaudiere fioul standard", "chaudiere fioul")
+    df = replace_value(df, "type_generateur_chauffage", "chaudiere gaz basse temperature", "chaudiere gaz")
+    df = replace_value(df, "type_generateur_chauffage", "chaudiere gaz condensation", "chaudiere gaz")
+    df = replace_value(df, "type_generateur_chauffage", "chaudiere gaz standard", "chaudiere gaz")
+    df = replace_value(df, "type_generateur_chauffage", "chaudiere gpl/butane/propane basse temperature", "chaudiere gpl/butane/propane")
+    df = replace_value(df, "type_generateur_chauffage", "chaudiere gpl/butane/propane condensation", "chaudiere gpl/butane/propane")
+    df = replace_value(df, "type_generateur_chauffage", "chaudiere gpl/butane/propane standard", "chaudiere gpl/butane/propane")
 
 
     #transform to binary data
@@ -161,11 +161,11 @@ def clean(chemin):
     if colonnes_listes=="scinde" :
         k=0
         for i in colonnesliste :
-            df = scinde_colonnes(df, i,modcolonneliste[k])
+            df = separate_columnes(df, i,modcolonneliste[k])
             k+=1
     else : 
         for i in colonnesliste :
-            df = convertir_listes_en_nombre(df, i)
+            df = liste_to_int(df, i)
 
     #replace na by "inconnu" if columns is object, else replace na by mean of the column
     df = conditional_fill_na(df)
@@ -175,7 +175,7 @@ def clean(chemin):
     
     #delete na
     for colonne in df.columns :
-        df=supprimer_lignes_na(df, colonne)
+        df=delete_na(df, colonne)
         
     # duplicate drop
     df.drop_duplicates(inplace = True)
