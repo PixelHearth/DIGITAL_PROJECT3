83439f791b75ce4babe7e20321eeb528f10636ec
gbar-dev <Guillaume.Baroin77@outlook.com>
2023-12-04 15:31:25 +0100

    cleaning

---
+++ src/data/pickle.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import pickle
+
+def stock_pickle_csv(path_in:str, path_out:str):
+
+    """
+    Stocke en pickle le fichier csv mis en entrée
+
+    var:
+    path_in (str): chemin du fichier csv en entrée
+    path_out (str): chemin du fichier pkl en sortie
+    """
+
+    # ouverture des données
+    df = pd.read_csv(path_in, sep = ",")
+
+    # écriture du pickle avec les données csv
+    with open(path_out, "wb") as file_pickle:
+        pickle.dump(df, file_pickle)
+
+def open_pkl(path_in:str):
+    # test ouverture d'un pickle
+    with open(path_in, "rb") as file2:
+        return(pickle.load(file2))
