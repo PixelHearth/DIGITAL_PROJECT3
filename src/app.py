e3ce30a447eb60bb8c44f9b7220f81380da9967c
gbar-dev <Guillaume.Baroin77@outlook.com>
2023-12-05 17:42:41 +0100

    changement for vba

--- src/app.py
+++ src/app.py
@@ -3,28 +3,21 @@ import time
 from sklearn.preprocessing import StandardScaler
 
 from data.clean import clean_df
-from data.preprocessing import CustomProcessing
+from data.preprocessing import CustomProcessing, ScalerProcessor
 from models.train_model import Models
 from models.selection import select_features
 from visualization.importance_feature_graph import plot_feature_importance
 from data.make_dataset import *
 
 def app():
-    # Calculate the start time
-    start = time.time()
-
     # Import database
     properties = clean_df("src/data/database/df_clean.csv")
     
     # Import customer desc
-    customer = importation_excel("src/formulaire.xlsm", "Source")
+    customer = importation_excel("src/formulaire 1.xlsm", "Source")
     
-    # Instance StandardScaler for the models
-    scaler = StandardScaler()
-    numerous = properties.select_dtypes(include=['number']).columns
-    # Apply scaler to numeric columns
-    properties[numerous] = scaler.fit_transform(properties[numerous])
-    # customer[customer.select_dtypes(include=['number']).columns] = scaler.fit_transform(customer.select_dtypes(include=['number']).columns)
+    # Instance StandardScaler for the models and run
+    ScalerProcessor(properties,customer).run_processing_pipeline()
     
     # Instance of the processing framework and data training on properties for encoding
     cpp_p_selection = CustomProcessing(properties)
@@ -32,51 +25,20 @@ def app():
 
     # Selection of number of features, important variables, encoding must have been done before
     nb_features = 15
-    columns_important, importance = select_features(properties, nb_features)
-    print(columns_important)
+    properties_selected, importance = select_features(properties, nb_features)
+    
     # Plot of features importances
     plot_feature_importance(importance, nb_features)
 
-    # Get columns keeped by select features
-    col = cpp_p_selection.column_selection(columns_important)
-    # Inverse transform the properties 
-    cpp_p_selection.inverse_transform(properties)
-
-    # Select col in initial properties with col selected by the RandomForest
-    if 'classe_bilan_dpe' in col:
-        # Keep classe_bilan_dpe and col in randomforest
-        ordered_columns = ['classe_bilan_dpe'] + [col for col in col if col != 'classe_bilan_dpe']
-
-        # copy property with new cols
-        properties_selected = properties[ordered_columns]
-    
-    customer = properties_selected.sample(1)
-    
-    # Instance processing framework and data training on properties for encoding after selection
-    cpp_kneigh = CustomProcessing(properties_selected)
-
-    #fit and transform training dataset and customer dataset
-    properties_selected = cpp_kneigh.fit_transform(properties_selected)
-    customer = cpp_kneigh.transform(customer)
     # Instance and training of k_neighbors on the encoded data
     knn_model = Models(properties_selected, customer)
     
-    # get k optimal
-    best_k = knn_model.metric_knn()
-    
     # Make the K Nearest Neighbors
-    individual,proba = knn_model.k_neighbors(best_k)
-    # Restoration of a human-readable dataframe
-    # Predicted value of k_neighbors
-    cpp_kneigh.inverse_transform(individual)
-
-    # Export proba in excel sheet
-    export_excel(proba,"src/formulaire.xlsm", "Source")
+    proba,score = knn_model.k_neighbors()
     
-    # Calculate the total execution time
-    end = time.time()
-    print("Execution time: ", round(end - start, 2), "seconds") 
-    return individual
+    print(f"La probabilité d'être dans une des 3 classes est de, {score.flatten()}")
+    # Export proba in excel sheet
+    export_excel(proba,"src/formulaire.xlsm 1", "Source")
     
 if __name__ == "__main__":
     app()
