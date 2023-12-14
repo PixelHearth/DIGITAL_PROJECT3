25f17a2153a7c3d5bfba6ed990cddef7e65148a9
gbar-dev <baroin.guillaume.pro@gmail.com>
2023-12-14 22:17:33 +0100

    optimisation app

--- src/app.py
+++ src/app.py
@@ -14,8 +14,8 @@ def app():
     properties = clean_df("C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/src/data/database/df_clean.csv")
     
     # Import customer desc
-    customer = importation_excel("C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/src/formulaire.xlsm", "Source")
-    print(customer)
+    customer = importation_excel("C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/src/formulaire (1).xlsm", "Source")
+    
     # Instance StandardScaler for the models and run
     ScalerProcessor(properties,customer).run_processing_pipeline()
     
@@ -26,8 +26,9 @@ def app():
     # Selection of number of features, important variables, encoding must have been done before
     nb_features = 15
     properties_selected, importance = select_features(properties, nb_features)
+
     # Plot of features importances
-    plot_feature_importance(importance, nb_features)
+    # plot_feature_importance(importance, nb_features)
 
     # Instance and training of k_neighbors on the encoded data
     knn_model = Models(properties_selected, customer)
@@ -35,15 +36,9 @@ def app():
     # Make the K Nearest Neighbors
     proba,score = knn_model.k_neighbors()
     
-    print(f"La probabilité d'être dans une des 3 classes est de, {score.flatten()}")
-    print(proba)
-    # Export proba in excel sheet
-
-    # Écrivez les données dans le fichier texte
+    # Write data on txt file
     with open("C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/src/data/database/prediction.txt", 'w') as fichier:
         fichier.write(str(proba))
 
-    # export_excel(proba,"C:/Users/Guillaume Baroin/Documents/M2_sep/DIGITAL_PROJECT3/src/formulaire.xlsm", "Source")
-    
 if __name__ == "__main__":
     app()
